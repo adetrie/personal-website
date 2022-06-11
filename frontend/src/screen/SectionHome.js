1bd9916b3de1f0f0885a2430172e959feba49549
Alexis Détrie <detrie.alexis@gmail.com>
2022-06-11 20:13:16 +0200

    Add some information on Home section

--- frontend/src/screen/SectionHome.js
+++ frontend/src/screen/SectionHome.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import '../App.css';
+import Typewriter from 'typewriter-effect';
+
 
 const SectionHome = () => {
 
 
     return (
-        <div></div>
+        <div className="flex items-center justify-center h-screen">
+            <div>
+                <h1 className="text-7xl font-bold mb-10 text-center">
+                    Salut, je suis Alexis.
+                </h1>
+
+                <div className="inline-block align-top w-full text-center">
+                    <h2 className="text-3xl inline-block">
+                        Developer et Architecte en Java.
+                    </h2>
+                </div>
+
+                <div className="inline-block align-top w-full text-center">
+                    <h2 className="text-3xl inline-block">
+                        Je suis&nbsp;
+                        <Typewriter
+                            wrapperClassName='float: right'
+                            options={{
+                                strings: ['un développeur Java', 'un architecte Java', 'un développeur React', 'un développeur JEE/Jakarta', 'un développeur Spring'],
+                                autoStart: true,
+                                loop: true,
+                            }}
+                        />
+                    </h2>
+                </div>
+            </div>
+        </div>
     );
 }
 
