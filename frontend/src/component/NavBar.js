4e1d55945d3a42379ba4bbdc0b71f99fd8810292
Alexis Détrie <detrie.alexis@gmail.com>
2022-06-13 20:26:01 +0200

    Disable darkmode

--- frontend/src/component/NavBar.js
+++ frontend/src/component/NavBar.js
@@ -8,37 +8,37 @@ const NavBar = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <nav className="bg-white shadow dark:bg-gray-800">
+        <nav className="bg-white shadow">
             <div
-                className="container flex items-center justify-center  mx-auto text-gray-600 capitalize dark:text-gray-300">
+                className="container flex items-center justify-center  mx-auto text-gray-600 capitalize">
                 <div className="flex items-center justify-between h-16">
                     <div className="hidden md:block">
                         <div
-                            className="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
+                            className="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize ">
                             <a href="#"
-                               className="text-gray-800 transition-colors duration-200 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">home</a>
+                               className="text-gray-800 transition-colors duration-200 transform border-b-2 border-blue-500 mx-1.5 sm:mx-6">home</a>
 
                             <a href="#"
-                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">About</a>
+                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform hover:border-blue-500 mx-1.5 sm:mx-6">About</a>
 
                             <a href="#"
-                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Skills</a>
+                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform hover:border-blue-500 mx-1.5 sm:mx-6">Skills</a>
 
                             <a href="#"
-                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Experiences</a>
+                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform hover:border-blue-500 mx-1.5 sm:mx-6">Experiences</a>
 
                             <a href="#"
-                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">School</a>
+                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform hover:border-blue-500 mx-1.5 sm:mx-6">School</a>
 
                             <a href="#"
-                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">My
+                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform hover:border-blue-500 mx-1.5 sm:mx-6">My
                                 personal page</a>
 
                             <a href="#"
-                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Contact</a>
+                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform hover:border-blue-500 mx-1.5 sm:mx-6">Contact</a>
 
                             <a href="#"
-                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">
+                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform hover:border-blue-500 mx-1.5 sm:mx-6">
                                 <svg
                                     className="w-6 h-6 text-blue-500 fill-current"
                                     xmlns="http://www.w3.org/2000/svg"
@@ -50,7 +50,7 @@ const NavBar = () => {
                             </a>
 
                             <a href="#"
-                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">
+                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform hover:border-blue-500 mx-1.5 sm:mx-6">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                                     <path
                                         d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
@@ -118,29 +118,29 @@ const NavBar = () => {
                     <div className="md:hidden" id="mobile-menu">
                         <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
                            <a href="#"
-                               className="text-gray-800 transition-colors duration-200 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6 block">home</a>
+                               className="text-gray-800 transition-colors duration-200 transform border-b-2 border-blue-500 mx-1.5 sm:mx-6 block">home</a>
 
                             <a href="#"
-                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6 block">About</a>
+                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform hover:border-blue-500 mx-1.5 sm:mx-6 block">About</a>
 
                             <a href="#"
-                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6 block">Skills</a>
+                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform hover:border-blue-500 mx-1.5 sm:mx-6 block">Skills</a>
 
                             <a href="#"
-                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6 block">Experiences</a>
+                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform hover:border-blue-500 mx-1.5 sm:mx-6 block">Experiences</a>
 
                             <a href="#"
-                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6 block">School</a>
+                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform hover:border-blue-500 mx-1.5 sm:mx-6 block">School</a>
 
                             <a href="#"
-                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6 block">My
+                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform hover:border-blue-500 mx-1.5 sm:mx-6 block">My
                                 personal page</a>
 
                             <a href="#"
-                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6 block">Contact</a>
+                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform hover:border-blue-500 mx-1.5 sm:mx-6 block">Contact</a>
 
                             <a href="#"
-                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 mx-1.5 sm:mx-6 block">
+                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform mx-1.5 sm:mx-6 block">
                                 <svg
                                     className="w-6 h-6 text-blue-500 fill-current"
                                     xmlns="http://www.w3.org/2000/svg"
@@ -152,7 +152,7 @@ const NavBar = () => {
                             </a>
 
                             <a href="#"
-                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 mx-1.5 sm:mx-6 block">
+                               className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform mx-1.5 sm:mx-6 block">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                                     <path
                                         d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
