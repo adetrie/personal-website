3bfe8f9fd5b1ab0176ed7abf4f64c7316f8d775a
Alexis Détrie <detrie.alexis@gmail.com>
2022-06-14 20:27:39 +0200

    Some fix concerning responsive design

--- frontend/src/screen/SectionHome.js
+++ frontend/src/screen/SectionHome.js
@@ -13,19 +13,19 @@ const SectionHome = () => {
         <div id="section-home" className="sectionbody min-h-screen flex items-center justify-center">
             <div className="worklet-canvas"></div>
             <div
-                className="backdrop-blur-xl text-white m-10 h-auto py-20 px-10 w-2/4 mx-auto rounded-3xl shadow-xl hover:rotate-1 transition-transform border-white border-solid">
-                <h1 className="text-7xl font-bold mb-10 text-center">
+                className="backdrop-blur-xl text-white lg:m-10 md:m-3 h-auto py-10 px-7 lg:py-20 lg:px-10 w-2/4 mx-auto rounded-3xl shadow-xl hover:rotate-1 transition-transform border-white border-solid">
+                <span className="text-4xl lg:text-7xl font-bold mb-10 text-center">
                     Salut 👋, je suis Alexis.
-                </h1>
+                </span>
 
                 <div className="inline-block align-top w-full text-center">
-                    <h2 className="text-3xl inline-block">
+                    <h2 className="lg:text-3xl text-2xl inline-block">
                         Développeur et Architecte en Java.
                     </h2>
                 </div>
 
                 <div className="inline-block align-top w-full text-center">
-                    <h2 className="text-3xl inline-block">
+                    <h2 className="lg:text-3xl text-2xl inline-block">
                         Je suis&nbsp;
                         <div className="float-right">
                             <Typewriter
